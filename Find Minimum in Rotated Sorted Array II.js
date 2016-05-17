f7703f4d080522f0ef93f05bceeace7d89c0308f
Tanxin Du <xin24914@gmail.com>
2016-05-17 00:40:20 -0400

    added two leetcode exercises

---
+++ Find Minimum in Rotated Sorted Array II.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    var len = nums.length;
+    
+    if(len === 0){
+        return 0;
+    }else if(len === 1) {
+        return nums[0];
+    }else if(len ===2) {
+        return Math.min(nums[0], nums[1]);
+    }
+    
+    var start = 0;
+    var stop = len - 1;
+    
+    while(start < stop -1) {
+        if(nums[start] < nums[stop]) {
+            return nums[start];
+        }
+        
+        var mid = Math.floor(start + (stop-start)/2);
+        if(nums[mid] > nums[start]) {
+            start = mid;
+        } else if(nums[mid] < nums[start]) {
+            stop = mid;
+        } else if(nums[mid] === nums[start]) {
+            start++;
+        }
+    }
+    
+    return Math.min(nums[start], nums[stop]);
+};
--- Largest Rectangle in Histogram.js
+++ Largest Rectangle in Histogram.js
@@ -4,4 +4,26 @@
  */
 var largestRectangleArea = function(heights) {
     
+    if (heights === null || heights.length === 0) {
+        return 0;
+    }
+    
+    var stack = [];
+    
+    var max = 0;
+    var i = 0;
+    
+    while(i<heights.length){
+        if(stack.length === 0 || heights[i] >= stack[stack.length-1]) {
+            stack.push(i);
+            i++;
+        } else {
+            var p = stack.pop();
+            var h = heights[p];
+            var w = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            max = Math.max(h*w, max);
+        }
+    }
+    
+    return max;
 };
