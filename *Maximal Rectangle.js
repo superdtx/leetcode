1d31c52d62a8f77979581374c5c8a8b15d86f756
superdtx <xin24914@gmail.com>
2016-05-17 10:02:24 -0400

    Create *Maximal Rectangle.js

---
+++ *Maximal Rectangle.js
@@ -0,0 +1,56 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function(matrix) {
+    console.log('come here');
+
+    if(matrix.length === 0 || matrix[0].length === 0) {
+        return 0;
+    }
+
+    var m = matrix.length;
+    var n = matrix[0].length;
+    var height = [];
+    
+    for(var i = 0; i < m; i++) {
+        for(var j = 0; j < n; j++) {
+            if(matrix[i][j] === '0') {
+                height[i][j] = 0;
+            } else {
+                height[i][j] = (i === 0? 1:height[i-1][j] + 1);
+            }
+        }
+    }
+    
+    var maxArea = 0;
+    for(var i = 0; i < m; i++) {
+        maxArea = Math.max(maxArea, largestRectangleArea(height[i]));
+    }
+    
+    return maxArea;
+};
+
+var largestRectangleArea = function(heights) {
+  
+  if(heights.length === 0) {
+      return 0;
+  }
+  
+  var stack = [];
+  var max = 0;
+  var leftarea = 0, rightarea = 0;
+  
+  for(var i=0; i < heights.length; i++) {
+      while(stack.length !== 0 && heights[stack[stack.length-1]] > heights[i]) {
+          var tmp = stack[stack.length-1];
+          stack.pop();
+          leftarea = (stack.length === 0? tmp + 1: tmp - heights[stack[stack.length-1]])*heights[tmp];
+          rightarea = (i - tmp - 1)* height[tmp];
+          max = Math.max(max, (leftarea+rightarea));
+      }
+      stack.push(i);
+  }
+  
+  return max;  
+};
