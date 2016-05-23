dc70a79386a31abd3e3d343c54937ba48011f9a1
superdtx <xin24914@gmail.com>
2016-05-23 16:50:34 -0400

    Update Search for a Range.js

--- Search for a Range.js
+++ Search for a Range.js
@@ -6,3 +6,47 @@ For example,
 Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    
+    var result = [],
+        leftIndex = findLeftBoundry(nums, target),
+        rightIndex = findRightBoundry(nums, target);
+    
+    result.push(leftIndex);    
+    result.push(rightIndex);
+    
+    return result;
+
+};
+
+var findLeftBoundry = function(nums, target) {
+    var i = 0;
+    
+    while(i < nums.length) {
+        if (nums[i] === target) {
+            return i;
+        }
+        i++;
+    }
+    
+    return -1;
+};
+
+var findRightBoundry = function(nums, target) {
+    var i = nums.length-1;
+    
+    while(i >= 0) {
+        if (nums[i] === target) {
+            return i;
+        }
+        i--;
+    }
+    
+    return -1;
+};
