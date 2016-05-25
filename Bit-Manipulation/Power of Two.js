cee419eed4f7a2ddf7072b5974eff1f019f749a3
superdtx <xin24914@gmail.com>
2016-05-25 17:55:53 -0400

    Update Power of Two.js

--- Bit-Manipulation/Power of Two.js
+++ Bit-Manipulation/Power of Two.js
@@ -13,3 +13,20 @@ var isPowerOfTwo = function(n) {
     
     return isPowerOfTwo(n/2);
 };
+
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    
+    while(n>=2){
+        if(n%2!==0){
+            break;
+        }
+        n = n / 2;
+    }
+    
+    return n === 1;
+};
