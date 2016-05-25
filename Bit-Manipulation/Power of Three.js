da6dee65331740c2700845621dd376a259e7eca4
superdtx <xin24914@gmail.com>
2016-05-25 18:01:46 -0400

    Update Power of Three.js

--- Bit-Manipulation/Power of Three.js
+++ Bit-Manipulation/Power of Three.js
@@ -13,3 +13,21 @@ var isPowerOfThree = function(n) {
     return isPowerOfThree(n/3);
     
 };
+
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    
+    while(n>=3){
+        if(n%3!==0){
+            break;
+        }
+        n = n/3;
+    }
+    
+    return n===1;
+    
+};
