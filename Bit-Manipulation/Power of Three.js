7f9178d854e426d6f4cd2bcf89458bf4a6051a5f
superdtx <xin24914@gmail.com>
2016-05-25 17:59:07 -0400

    Create Power of Three.js

---
+++ Bit-Manipulation/Power of Three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    
+    if(n===1){
+        return true;
+    }
+    if(n===0 || n%3!==0){
+        return false;
+    }
+    return isPowerOfThree(n/3);
+    
+};
