763aaeee717ee9b5303a40e788b79fa9d28ce35c
superdtx <xin24914@gmail.com>
2016-06-08 21:57:40 -0400

    Update Binary Search Tree.js

--- Data-Structure/Binary Search Tree.js
+++ Data-Structure/Binary Search Tree.js
@@ -1,1 +1,36 @@
+// https://www.nczonline.net/blog/2009/06/09/computer-science-in-javascript-binary-search-tree-part-1/
 
+function Node(val){
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+function BinarySearchTree(){
+  this.root = null;
+}
+
+BinarySearchTree.prototype = {
+
+    //restore constructor
+    constructor: BinarySearchTree,
+
+    add: function (value){
+    },
+
+    contains: function(value){
+    },
+
+    remove: function(value){
+    },
+
+    size: function(){
+    },
+
+    toArray: function(){
+    },
+
+    toString: function(){
+    }
+
+};
