58698bb62b444d9d2edea1593d8e4a0ef6d1ef93
superdtx <xin24914@gmail.com>
2016-06-16 17:51:13 -0400

    Create Binary Tree Level Order Traversal II.js

---
+++ Tree/Binary Tree Level Order Traversal II.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    var height = getHeight(root);
+    if(height === 0){
+        return [];
+    }
+    var result = [];
+    for(var i=0; i<height; i++){
+        result.push([]);
+    }
+    
+    reverseRetrival(root, result, height-1);
+    return result;
+};
+
+function reverseRetrival(root, result, level){
+    if(root === null){
+        return;
+    }
+    
+    result[level].push(root.val);
+    reverseRetrival(root.left, result, level-1);
+    reverseRetrival(root.right, result, level-1);
+}
+
+function getHeight(root){
+    if(root === null){
+        return 0;
+    }
+    var left = getHeight(root.left);
+    var right = getHeight(root.right);
+    return Math.max(left, right)+1;
+}
