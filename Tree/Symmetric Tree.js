c040ab564b30b75d9ec606b582038d53170fb2a2
superdtx <xin24914@gmail.com>
2016-06-20 18:20:01 -0400

    Update Symmetric Tree.js

--- Tree/Symmetric Tree.js
+++ Tree/Symmetric Tree.js
@@ -32,3 +32,49 @@ function recursiveTraversal(left, right){
     return cond1&&cond2&&cond3;
     
 }
+
+/**
+ * iteratively
+ */
+var isSymmetric = function(root) {
+    if(root === null){
+        return true;
+    }  
+    
+    var leftNode = root.left;
+    var rightNode = root.right;
+    
+    if(!leftNode&&!rightNode){
+        return true;
+    }
+    
+    if((!leftNode&&rightNode)||(leftNode&&!rightNode)){
+        return false;
+    }
+    
+    var queue1 = [];
+    var queue2 = [];
+    
+    queue1.push(leftNode);
+    queue2.push(rightNode);
+    while(queue1.length !== 0 && queue2.length !== 0){
+        var temp1 = queue1[0];
+        var temp2 = queue2[0];
+        queue1.shift();
+        queue2.shift();
+        if((!temp1&&temp2)||(temp1&&!temp2)){
+            return false;
+        }
+        if(temp1&&temp2){
+            if(temp1.val !== temp2.val){
+                return false;
+            }
+            queue1.push(temp1.left);
+            queue1.push(temp1.right);
+            queue2.push(temp2.right);
+            queue2.push(temp2.left);
+        }
+
+    }
+    return true;
+};
