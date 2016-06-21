ff98d3789b986737e0ba3902d403db1e8b6829f2
superdtx <xin24914@gmail.com>
2016-06-21 11:43:45 -0400

    Create Path Sum.js

---
+++ Tree/Path Sum.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if(root === null){
+        return false;
+    }
+    return DFS(sum, 0, root);
+};
+
+function DFS(target, sum, root) {
+    if(root === null){
+        return false;
+    }
+    sum += root.val;
+    if(root.left === null && root.right === null){
+        if(sum === target){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    var left = DFS(target, sum, root.left);
+    var right = DFS(target, sum, root.right);
+    
+    return left||right;
+}
