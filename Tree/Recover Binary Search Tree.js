d55cea7fa7be13309c6c64f9d5ef305b8f483b24
superdtx <xin24914@gmail.com>
2016-06-28 16:10:08 -0400

    Update Recover Binary Search Tree.js

--- Tree/Recover Binary Search Tree.js
+++ Tree/Recover Binary Search Tree.js
@@ -43,3 +43,42 @@ function treeToArray(root, array){
     array.push([root.val, root]);
     treeToArray(root.right, array);
 }
+
+// Solution O(1) space
+//因为inorder traversal 叶子的值会越来越大
+//所以存储前驱叶子 使之与现在的叶子进行比较
+var recoverTree = function(root) {
+    
+    let array = [];
+    
+    inorderTraverse(root, array);
+    
+    let node1 = array[1];
+    let node2 = array[2];
+    
+    let temp = node1.val;
+    node1.val = node2.val;
+    node2.val = temp;
+    
+};
+
+function inorderTraverse(root, array){
+    if(!root){
+        return;
+    }
+    
+    inorderTraverse(root.left, array);
+    
+    if(array[0] && array[0].val > root.val){
+        if(!array[1]) {
+            array[1] = array[0];
+            array[2] = root;
+        } else {
+            array[2] = root;
+        }
+    }
+    
+    array[0] = root;
+    
+    inorderTraverse(root.right, array);
+}
