da45c60a9653711f068cb3b79862d16bc94f3176
superdtx <xin24914@gmail.com>
2016-07-24 13:09:37 -0400

    Create Subsets.js

---
+++ Backtracking/Subsets.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    
+    var result = [];
+    var len = nums.length;
+    
+    nums.sort(function(a, b){
+        return a - b;
+    });
+    
+    helper(nums, 0, len-1, [], result);
+    
+    return result;
+};
+
+function helper(nums, start, end, curArr, result) {
+    result.push(curArr);
+    
+    for(var i = start; i <= end; i++){
+        curArr.push(nums[i]);
+        helper(nums, i+1, end, curArr.concat(), result);
+        curArr.pop();
+    }
+}
