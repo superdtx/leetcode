e33a9d0249363b255473bad124a7a5db7207c1e6
superdtx <xin24914@gmail.com>
2016-09-15 13:41:08 -0400

    Create Intersection of Two Arrays II.js

---
+++ HashMap/Intersection of Two Arrays II.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    var res = [];
+    
+    nums1.map(function(elem){
+        if(nums2.indexOf(elem) > -1){
+            res.push(elem);
+        }
+    });
+    
+    return res;
+};
