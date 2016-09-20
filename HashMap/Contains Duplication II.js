26484ed3fc31cbf8aa6c1bc1b1f93e54aaa844fc
superdtx <xin24914@gmail.com>
2016-09-19 21:01:56 -0400

    Update Contains Duplication II.js

--- HashMap/Contains Duplication II.js
+++ HashMap/Contains Duplication II.js
@@ -1,4 +1,8 @@
 /**
+ * 112ms
+ */
+
+/**
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
@@ -25,4 +29,33 @@ var containsNearbyDuplicate = function(nums, k) {
     });
     
     return contains;
+};
+
+/**
+ * 85ms
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    var map = {},
+        len = nums.length,
+        i;
+    
+    for (i = 0; i < len; i++) {
+        if (map.hasOwnProperty(nums[i])) {
+            if (i - map[nums[i]] <= k) {
+                return true;
+            }
+            
+            map[nums[i]] = i;
+        } else {
+            map[nums[i]] = i;
+        }
+    }
+    
+    return false;
 };
