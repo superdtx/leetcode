e502a19827b34ed7ba575ec4d9be272f9be2dc21
superdtx <xin24914@gmail.com>
2016-09-27 11:02:43 -0400

    Update Remove Nth Node From End of List.js

--- Linked-List/Remove Nth Node From End of List.js
+++ Linked-List/Remove Nth Node From End of List.js
@@ -26,11 +26,8 @@ var removeNthFromEnd = function(head, n) {
         cur = cur.next;
     }
     
-    if(!cur.next.next){
-        cur.next = null;
-    } else {
-        cur.next = cur.next.next;
-    }
+    cur.next = cur.next.next;
+
     return dummy.next;
 };
 
@@ -63,10 +60,37 @@ var removeNthFromEnd = function(head, n) {
         cur1 = cur1.next;
     }
     
-    if(!cur1.next.next){
-        cur1.next = null;
-    } else {
-        cur1.next = cur1.next.next;
+    cur1.next = cur1.next.next;
+
+    return dummy.next;
+};
+
+/** 一个loop
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+    var dummy = new ListNode(0);
+    dummy.next = head;
+    var cur1 = dummy;
+    var cur2 = dummy;
+    
+    while(cur2.next){
+        cur2 = cur2.next;
+        n--;
+        (n < 0) && (cur1 = cur1.next);
     }
+    
+    cur1.next = cur1.next.next;
+    
     return dummy.next;
 };
