6c9af58d3099bd1784f20b3bade1c40f57e234c7
superdtx <xin24914@gmail.com>
2017-03-07 21:07:23 -0500

    Create 215. Kth Largest Element in an Array.js

---
+++ JS/215. Kth Largest Element in an Array.js
@@ -0,0 +1,25 @@
+/*
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+*/
+
+
+//O(N lg N) running time + O(1) memory
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    var len = nums.length;
+    nums.sort(function(a, b){
+        return a - b;
+    });
+    return nums[len - k];
+};
+
