030a447c0c9acd751e40fcdc6e3def87d973df8f
superdtx <xin24914@gmail.com>
2017-03-07 23:40:01 -0500

    Create 285. Inorder Successor in BST.js

---
+++ JS/285. Inorder Successor in BST.js
@@ -0,0 +1,28 @@
+//Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    
+    if(root === null){
+        return null;
+    }
+    
+    if(root.val <= p.val){
+        return inorderSuccessor(root.right, p);
+    } else {
+        var left = inorderSuccessor(root.left, p);
+        return (left !== null)? left: root;
+    }
+    
+};
