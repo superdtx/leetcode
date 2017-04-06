aec15577086897da02eabd3447e5aad3eff7f971
superdtx <xin24914@gmail.com>
2017-04-06 15:13:26 -0400

    Create Fizz Buzz.py

---
+++ Python/Fizz Buzz.py
@@ -0,0 +1,37 @@
+'''
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+'''
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return ['Fizz'*(not i%3) + 'Buzz'*(not i%5) + str(i)*((i%3 and i%5) > 0) for i in range(1, n+1)]
+        
