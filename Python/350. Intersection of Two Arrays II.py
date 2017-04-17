d98b768552caf54ec995cf9a3cfb85f4f60f10cf
superdtx <xin24914@gmail.com>
2017-04-17 10:54:29 -0400

    Create 350. Intersection of Two Arrays II.py

---
+++ Python/350. Intersection of Two Arrays II.py
@@ -0,0 +1,41 @@
+'''
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+'''
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set1 = collections.Counter(nums1)
+        set2 = collections.Counter(nums2)
+        ret = []
+        set = set1 & set2
+        for id in set : 
+            for i in xrange(0, set[id]):
+                ret.append(id)
+                
+        return ret
+        
+       
+from collections import Counter
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        c1, c2 = Counter(nums1), Counter(nums2)
+        return sum([[num] * min(c1[num], c2[num]) for num in c1 & c2], [])       
+       
+      
