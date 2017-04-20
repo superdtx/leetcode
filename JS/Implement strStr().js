e331064fa3010a0430ac8db845d0ae8265178196
superdtx <xin24914@gmail.com>
2017-04-19 22:29:44 -0400

    Update Implement strStr().js

--- JS/Implement strStr().js
+++ JS/Implement strStr().js
@@ -10,3 +10,27 @@
 var strStr = function(haystack, needle) {
     return haystack.indexOf(needle);
 };
+
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+
+    var i, j;
+    
+    for(i = 0; i < haystack.length - needle.length + 1; i++){
+        for(j = 0; j < needle.length; j++){
+            if(haystack[i+j] !== needle[j]){
+                break;
+            }
+        }
+        if(j === needle.length){
+            return i;
+        }
+    }
+    
+    return -1;
+};
