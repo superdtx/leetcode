0242de17aa94baf41bb7c0fe7bb662a071569c32
superdtx <xin24914@gmail.com>
2017-05-10 14:55:56 -0400

    Update 500. Keyboard Row.js

--- JS/500. Keyboard Row.js
+++ JS/500. Keyboard Row.js
@@ -42,3 +42,22 @@ var inOneRow = function(word, rows){
     }
     return true;
 };
+
+
+
+/**
+regex
+**/
+
+var findWords = function(words) {
+    return words.filter((w) => {
+        // remove word from array if it fails matching all three rows
+        if (
+            !/^[qwertyuiop]*$/i.test(w) &&
+            !/^[asdfghjkl]*$/i.test(w) &&
+            !/^[zxcvbnm]*$/i.test(w)
+        ) return false;
+        
+        return true;
+    });
+};
