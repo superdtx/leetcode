09c7ce9eee7902cd1a4c1e7e944c7f4b8943071e
superdtx <xin24914@gmail.com>
2017-05-10 13:55:42 -0400

    Create 500. Keyboard Row.js

---
+++ JS/500. Keyboard Row.js
@@ -0,0 +1,44 @@
+/**
+Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+
+Example 1:
+Input: ["Hello", "Alaska", "Dad", "Peace"]
+Output: ["Alaska", "Dad"]
+Note:
+You may use one character in the keyboard more than once.
+You may assume the input string will only contain letters of alphabet.
+**/
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    var rows = [['q','w','e','r','t','y','u','i','o','p'], ['a','s','d','f','g','h','j','k','l'],['z','x','c','v','b','n','m']];
+    var res = [];
+    
+    for(var i = 0; i < words.length; i++){
+        if(inOneRow(words[i], rows)){
+            res.push(words[i]);
+        }
+    }
+    
+    return res;
+};
+
+var inOneRow = function(word, rows){
+    var i;
+    var wordinLow = word.toLowerCase();
+    for(i = 0; i < rows.length; i++){
+        if(rows[i].indexOf(wordinLow[0]) !== -1){
+            break;
+        }
+    }
+    
+    for(var j = 0; j < wordinLow.length; j++){
+        if(rows[i].indexOf(wordinLow[j]) === -1){
+            return false;
+        }
+    }
+    return true;
+};
