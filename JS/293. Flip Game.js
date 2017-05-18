74d09ca0e3e97c24289e2e0a5e4495e3e5fe288a
superdtx <xin24914@gmail.com>
2017-05-18 14:22:58 -0400

    Create 293. Flip Game.js

---
+++ JS/293. Flip Game.js
@@ -0,0 +1,31 @@
+/**
+You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+Write a function to compute all possible states of the string after one valid move.
+
+For example, given s = "++++", after one move, it may become one of the following states:
+
+[
+  "--++",
+  "+--+",
+  "++--"
+]
+If there is no valid move, return an empty list [].
+**/
+
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var generatePossibleNextMoves = function(s) {
+    
+    var ret = [];
+    
+    for(var i = 0; i < s.length-1; i++){
+        if(s[i]==='+' && s[i+1]==='+'){
+            ret.push(s.slice(0, i)+'--'+s.slice(i+2));
+        }
+    }
+    
+    return ret;
+};
