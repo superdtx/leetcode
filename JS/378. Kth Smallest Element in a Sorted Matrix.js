9d41b9dbf771a3b3eec3c3e4c295128ee7072adf
superdtx <xin24914@gmail.com>
2017-06-20 10:15:00 -0400

    Update 378. Kth Smallest Element in a Sorted Matrix.js

--- JS/378. Kth Smallest Element in a Sorted Matrix.js
+++ JS/378. Kth Smallest Element in a Sorted Matrix.js
@@ -18,6 +18,18 @@ You may assume k is always valid, 1 ≤ k ≤ n2.
 **/
 
 /**
+Solution 2 : Binary Search
+We are done here, but let's think about this problem in another way:
+The key point for any binary search is to figure out the "Search Space". For me, I think there are two kind of "Search Space" -- index and range(the range from the smallest number to the biggest number). Most usually, when the array is sorted in one direction, we can use index as "search space", when the array is unsorted and we are going to find a specific number, we can use "range".
+
+Let me give you two examples of these two "search space"
+
+index -- A bunch of examples -- https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/ ( the array is sorted)
+range -- https://leetcode.com/problems/find-the-duplicate-number/ (Unsorted Array)
+The reason why we did not use index as "search space" for this problem is the matrix is sorted in two directions, we can not find a linear way to map the number and its index.
+**/
+
+/**
  * @param {number[][]} matrix
  * @param {number} k
  * @return {number}
