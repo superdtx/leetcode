a6ef744b7b35769077eab1a8b17c1e786a72a01b
superdtx <xin24914@gmail.com>
2017-10-27 15:12:56 -0400

    Update 657. Judge Route Circle.js

--- JS/657. Judge Route Circle.js
+++ JS/657. Judge Route Circle.js
@@ -30,3 +30,12 @@ var judgeCircle = function(moves) {
     
     return false;
 };
+
+
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    return (moves.split("L").length === moves.split("R").length) && (moves.split("U").length === moves.split("D").length);
+};
